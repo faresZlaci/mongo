2d7a14559b4d97370ce2536ad878743681316218
faresZlaci <133266884+faresZlaci@users.noreply.github.com>
2023-12-20 11:25:59 +0100

    Update mongo.js

--- Login-Signup-Form-main/src/mongo.js
+++ Login-Signup-Form-main/src/mongo.js
@@ -1,6 +1,5 @@
 const mongoose=require("mongoose")
 
-mongoose.connect("mongodb://uvrqiwahjpcsextetpun:uWqPP5rOJWNeFuAPaq07@n1-c2-mongodb-clevercloud-customers.services.clever-cloud.com:27017,n2-c2-mongodb-clevercloud-customers.services.clever-cloud.com:27017/b4x4mcqkjdboq4o?replicaSet=rs0")
 .then(()=>{
     console.log('mongoose connected');
 })
